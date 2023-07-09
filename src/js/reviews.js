3e8c1de76e571977a3ab9895f908e0ffce5f0a35
Illya-Bilyk <bilikillya.i@gmail.com>
2023-07-09 20:26:34 +0300

    20

---
+++ src/js/reviews.js
@@ -0,0 +1,14 @@
+// document.querySelector(document).ready(function () {
+//   document.querySelector(window).scroll(function () {
+//     var scrollPos = document.querySelector(this).scrollTop();
+//     document.querySelector('.revText').css({
+//       top: scrollPos / 2 + 'px',
+//       opacity: 1 - scrollPos / 510,
+//     });
+//     // if (document.querySelector(this).scrollTop() > 100) {
+//     //   document.querySelector('.revText').css({ opacity: '0' });
+//     // } else {
+//     //   document.querySelector('.revText').css({ opacity: '1.5' });
+//     // }
+//   });
+// });
