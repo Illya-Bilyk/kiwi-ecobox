d4a12b0cfed726799ad11dd32cdd6496e619c026
Illya-Bilyk <94546203+Illya-Bilyk@users.noreply.github.com>
2023-07-11 16:50:35 +0000

    Deploying to gh-pages from @ Illya-Bilyk/sheep-fish-test@23f801531db5cfa9dff3021eda0a047ad2205db8 🚀

---
+++ index.380be7ea.js
@@ -0,0 +1,2 @@
+new Swiper(".sample-slider",{loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.380be7ea.js.map
