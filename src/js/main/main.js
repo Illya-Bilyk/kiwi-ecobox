28301676c1bd5b2d843e310350099ebcd98b4b01
Illya-Bilyk <bilikillya.i@gmail.com>
2023-07-11 14:29:35 +0300

    9

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -2,24 +2,26 @@ import { images } from './imagesMain';
 import ellipseTransparent from '../../images/common/ellipseTransparent.svg';
 
 const refs = {
-  firstImages: document.querySelector('.firstImages'),
+  firstImages: document.querySelector('.firstImages-list'),
 };
 
 const markup = images
   .map(({ src, alt }) => {
     return `
-   <div class="imageItem">
-          <img
+   <div class="firstImages-item">
+      <div class="ellipseTr">  
+         <img
             src=${ellipseTransparent}
-            alt="ellipse Transparent"
-            class="ellipseTr"
+            alt="ellipse Transparent"   
           />
+      </div>
+      <div class="firstImages-item--image">
           <img
             src=${src}
-            alt=${alt}
-            class="itemImage"
+            alt=${alt}        
           />
-        </div>`;
+      </div>
+   </div>`;
   })
   .join('');
 
--- src/js/mobMenu.js
+++ src/js/mobMenu.js
@@ -3,12 +3,20 @@
     openMenuBtn: document.querySelector('.mob-menu-open'),
     closeMenuBtn: document.querySelector('.mob-menu-close'),
     menu: document.querySelector('.mob-menu'),
-    links: document.querySelector('.mob-menu--button'),
+    link1: document.querySelector('#mob-menu--button1'),
+    link2: document.querySelector('#mob-menu--button2'),
+    link3: document.querySelector('#mob-menu--button3'),
+    link4: document.querySelector('#mob-menu--button4'),
+    link5: document.querySelector('#mob-menu--button5'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
-  refs.links.addEventListener('click', toggleMenu);
+  refs.link1.addEventListener('click', toggleMenu);
+  refs.link2.addEventListener('click', toggleMenu);
+  refs.link3.addEventListener('click', toggleMenu);
+  refs.link4.addEventListener('click', toggleMenu);
+  refs.link5.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
