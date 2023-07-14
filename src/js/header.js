2637bdd80e48cd228b87a538b5c434d7f080de74
Illya-Bilyk <bilikillya.i@gmail.com>
2023-07-14 14:54:39 +0300

    6

--- src/js/header.js
+++ src/js/header.js
@@ -4,10 +4,18 @@ const mainRef = document.querySelector('#header');
 
 const header = ` 
       <ul class="header-list">
-        <li class="header-item"><a href="#main" class="mob-menu--link">HOME</a></li>
-        <li class="header-item"><a href="#news" class="mob-menu--link">BOXES</a></li>
         <li class="header-item">
-        <a href=''>
+           <button type="button" class="mob-menu--button">
+              <a href="#main" class="mob-menu--link">HOME</a>
+           </button>
+        </li>
+        <li class="header-item">
+          <button type="button" class="mob-menu--button">
+            <a href="#news" class="mob-menu--link">BOXES</a>
+          </button>
+        </li>
+        <li class="header-item">
+          <a href=''>
             <img
               src="${logo}"
               alt="logo"
@@ -15,8 +23,16 @@ const header = `
               height="100px"
           /></a>
         </li>
-        <li class="header-item"><a  href="#description" class="mob-menu--link">ABOUT</a></li>
-        <li class="header-item"><a  href="#subscribe" class="mob-menu--link">ACCOUNT</a></li>
+        <li class="header-item">
+          <button type="button" class="mob-menu--button">
+            <a  href="#description" class="mob-menu--link">ABOUT</a>
+          </button>
+        </li>
+        <li class="header-item">
+          <button type="button" class="mob-menu--button">
+            <a  href="#subscribe" class="mob-menu--link">ACCOUNT</a>
+          </button>
+        </li>
       </ul>
 
 `;
