c5953f171ea098f0e87eff444dd522daadbbe1b9
Illya-Bilyk <bilikillya.i@gmail.com>
2023-07-16 14:33:31 +0300

    1

--- src/js/subscribe/subscribe.js
+++ src/js/subscribe/subscribe.js
@@ -3,18 +3,8 @@ import { modal } from '../modal';
 const form = document.querySelector('#sub-form');
 
 function submitForm(event) {
-  form.validate({
-    rules: {
-      email: {
-        required: true,
-        email: true,
-      },
-    },
-  });
   modal(event.currentTarget.email.value);
-
   event.preventDefault();
-
   form.reset();
 }
 
